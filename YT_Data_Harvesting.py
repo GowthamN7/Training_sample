46553be8aae3b6a5f4a0b4d28e17aa3839fa49c9
GowthamN7 <132646100+GowthamN7@users.noreply.github.com>
2023-07-09 20:08:52 +0530

    Update YT_Data_Harvesting.py

--- YT_Data_Harvesting.py
+++ YT_Data_Harvesting.py
@@ -57,3 +57,64 @@ def get_playlists_data(youtube, channel_id):
   except HttpError as e:
     print("An error occurred:", e)
     return [], 0
+
+
+def get_video_data(youtube, playlist_id):
+  try:
+    playlist_items_request = youtube.playlistItems().list(
+      part = "snippet",
+      playlistId = playlist_id,
+      maxResults = 50
+    )
+
+    playlist_items_response = playlist_items_request.execute()
+
+      playlist_items = playlist_items_response.get("items", [])
+      video_ids = [item["snippet"]["resourceId"]["videoId"] for item in playlist_items]
+
+      videos_data = []
+      video_data_map = {}
+
+      for i in range(0, len(video_ids), 50):
+          video_ids_batch = video_ids[i:i+50]
+          video_request = youtube.videos().list(
+              part="snippet,contentDetails,statistics,status",
+              id=",".join(video_ids_batch)
+          )
+          video_response = video_request.execute()
+
+          videos = video_response.get("items", [])
+          for video in videos:
+              video_id = video["id"]
+              video_data = video_data_map.get(video_id)
+
+              if video_data is None:
+                  tags = video["snippet"].get("tags", [])
+                  duration = isodate.parse_duration(video["contentDetails"]["duration"]).total_seconds()
+
+                  video_data = {
+                      "video_id": video_id,
+                      "video_title": video["snippet"]["title"],
+                      "video_description": video["snippet"]["description"],
+                      "tags": tags,
+                      "published_at": video["snippet"]["publishedAt"],
+                      "view_count": video["statistics"]["viewCount"],
+                      "like_count": video["statistics"]["likeCount"],
+                      "dislike_count": 0,
+                      "favorite_count": video["statistics"]["favoriteCount"],
+                      "comment_count": video["statistics"]["commentCount"],
+                      "duration": duration,
+                      "thumbnail": video["snippet"]["thumbnails"]["default"]["url"],
+                      "caption_status": "Available" if video["contentDetails"]["caption"] else "Not Available",
+                      "comments": get_comments_data(youtube, video_id)
+                  }
+
+                  video_data_map[video_id] = video_data
+
+              videos_data.append(video_data)
+
+      return videos_data
+  except HttpError as e:
+      print("An HTTP error occurred:", e)
+      return []
+
