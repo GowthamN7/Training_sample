d54f5e2d65c338c1bf6000fe01969e3116480dd2
GowthamN7 <132646100+GowthamN7@users.noreply.github.com>
2023-07-09 19:46:06 +0530

    Update YT_Data_Harvesting.py

--- YT_Data_Harvesting.py
+++ YT_Data_Harvesting.py
@@ -13,3 +13,18 @@ def youtube_api_connect(api_key):
 
 def get_channel_data(youtube, channel_id):
   try:
+    channel_request = youtube.channels().list(
+      part = "sinppet, contentDetails, Statistics",
+      id = channel_id
+    )
+    channel_response = channel_request.execute()
+
+    if "items" in channel_response and len (channel_response["items"]) > 0:
+      channel_data = channel_response["items"][0]
+      return channel_data
+
+    else:
+      return None
+  except HttpError as e:
+    print("An error occurred:", e)
+    return None
