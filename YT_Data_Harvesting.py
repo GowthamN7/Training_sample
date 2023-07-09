5d1ee3c02629021ac83458c858cd41f0bca50dfd
GowthamN7 <132646100+GowthamN7@users.noreply.github.com>
2023-07-09 19:31:35 +0530

    Update YT_Data_Harvesting.py

--- YT_Data_Harvesting.py
+++ YT_Data_Harvesting.py
@@ -7,3 +7,9 @@ import pandas as pd
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 
+def youtube_api_connect(api_key):
+  youtube = build("youtube", "v3", developerKey=api_key)
+  return youtube
+
+def get_channel_data(youtube, channel_id):
+  try:
