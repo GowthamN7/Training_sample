e8e48f5e27bcce1de97ed0cfd9f6db9a0e8f8868
GowthamN7 <gowtham.p3100@gmail.com>
2023-08-31 22:58:26 +0530

    Apikey changed coz old apikey exeeded the quota

--- YT_Data_Harvesting.py
+++ YT_Data_Harvesting.py
@@ -500,7 +500,7 @@ def main():
         cursor = conn.cursor()
 
     st.subheader("Fetch Data")
-    apikey = "AIzaSyDE3sNFdXcs2gTpoRPUuKE67ZmBD4f1GAc"  
+    apikey = "AIzaSyAA_BVdBGeQsG0YplrCUAydNU14SUIbA3k"  
     st.write("To convert a channel name into a YouTube channel URL, you can use the following link:")
     st.write("[Convert Channel Name to Channel URL](https://commentpicker.com/youtube-channel-id.php)")
     st.write("After obtaining the channel URL, copy the channel ID from the URL and enter it below:")
